1ede398225beadb903e273db340ef0f529193641
Riscoh Tech <riscohfundzgmail.com>
2026-03-27 13:37:31 +0100

    done with the project as of now

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import About from "./componets/About";
+import Contact from "./componets/Contact";
+import Footer from "./componets/Footer";
 import Header from "./componets/Header";
 import Projects from "./componets/Projects";
 import Testimonials from "./componets/Testimonials";
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
+      <ToastContainer/>
       <Header/>
       <About/>
       <Projects/>
       <Testimonials/>
+      <Contact/>
+      <Footer/>
     </div>
   )
 }
